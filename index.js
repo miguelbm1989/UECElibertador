2acf5a029e2198a1b626143c5043ce4e3c00195e
miguelbm1989 <miguelbm1989@gmail.com>
2020-03-29 20:58:07 +0200

    adding stylesheet

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/landing', function(req,res){
     res.render('landing');
 });
 
